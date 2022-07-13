e1b3097dc96aa345df12516c1ce448adc49a6c76
Julia_Dia <50626230+juliadiaguileva@users.noreply.github.com>
2022-07-13 18:52:38 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$("#toggle").click(function () {
+  $(this).toggleClass("active");
+  $("#overlay").toggleClass("open");
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("gallery-item")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("gallery-modal")
+    );
+    myModal.show();
+  }
+});
